44f5304b2a21174978c0dafce02d0661d2accebd
Akshit Kumar Bansal <akbansal6204@gmail.com>
2026-02-05 18:49:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My React App on EC2</h1>
+        <p>Deploy by Akshit</p>
+      </div>
+    </>
+  )
+}
+
+export default App
